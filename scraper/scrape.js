397489f0bf98f91a954dca7cd90f9e4b3338b758
Sébastien Cevey <seb.cevey@guardian.co.uk>
2014-01-16 15:42:13 +0000

    scrape all

--- scraper/scrape.js
+++ scraper/scrape.js
@@ -4,34 +4,37 @@ var path = require('path');
 var http = require('http');
 var binPath = phantomjs.path;
 
-var source = 'guardian';
-var childArgs = [
-  path.join(__dirname, 'script.js'), source
-];
+var sources = ['guardian', 'nytimes', 'mailonline'];
 
-console.log("Source: ", source);
-childProcess.execFile(binPath, childArgs, function(err, stdout, stderr) {
-  var links = stdout;
-  console.log("Got " +links.length+ " links, posting...");
+sources.forEach(function(source) {
+  var childArgs = [
+    path.join(__dirname, 'script.js'), source
+  ];
 
-  var options = {
-    hostname: 'localhost',
-    port: 9000,
-    path: '/insert/' + source,
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    }
-  };
-  var post_req = http.request(options, function(res) {
-    res.setEncoding('utf8');
-    res.on('data', function (chunk) {
-      console.log('Response: ' + chunk);
+  console.log("Source: ", source);
+  childProcess.execFile(binPath, childArgs, function(err, stdout, stderr) {
+    var links = stdout;
+    console.log("Got " +links.length+ " links, posting...");
+
+    var options = {
+      hostname: 'localhost',
+      port: 9000,
+      path: '/insert/' + source,
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    };
+    var post_req = http.request(options, function(res) {
+      res.setEncoding('utf8');
+      res.on('data', function (chunk) {
+        console.log('Response: ' + chunk);
+      });
+      res.on('end', function() {
+        console.log("Sent");
+      });
     });
-    res.on('end', function() {
-      console.log("Sent");
-    });
+    post_req.write(links);
+    post_req.end();
   });
-  post_req.write(links);
-  post_req.end();
 });
