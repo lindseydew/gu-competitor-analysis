b27ffeca987825b9008294889f2f8a4c61e16d54
lindseydew <lindsey.dew@guardian.co.uk>
2014-01-16 14:47:42 +0000

    super simple mock up of the interface

--- app/models/NewsItem.scala
+++ app/models/NewsItem.scala
@@ -5,18 +5,20 @@ case class Position(width: Int, height: Int, xOffset: Int, yOffset: Int) {
 
 }
 
-case class Sources(name: String, items: NewsItem)
+case class Sources(name: String, items: List[NewsItem])
 case class NewsItem(headline: String, url: String, position: Position)
 
 object NewsItems {
-  def all = List(
-            NewsItem("NSA spying on us again", "http://www.gu.com/2134",
-            Position(380, 480, 0, 0)),
-            NewsItem("NSA spying on us again", "http://www.gu.com/2134",
-             Position(120, 60, 70, 100)),
-            NewsItem("NSA spying on us again", "http://www.gu.com/2134",
-             Position(30, 30, 500, 300))
-          ).sortBy(_.position.score)
-
-
+  def all = List(Sources("the guardian",List(
+                NewsItem("NSA spying on us again", "http://www.gu.com/2134",
+                         Position(380, 480, 0, 0)),
+                NewsItem("NSA spying on us again", "http://www.gu.com/2134",
+                        Position(120, 60, 70, 100)),
+                NewsItem("NSA spying on us again", "http://www.gu.com/2134",
+                        Position(30, 30, 500, 300))
+                ).sortBy(_.position.score)),
+                Sources("daily mail", List(
+                  NewsItem("BOOBS", "http://dailymail.CO.UK/boobs", Position(10000, 10000, 1, 3)))
+                  )
+                )
 }
