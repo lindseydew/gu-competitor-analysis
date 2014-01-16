90878c6485f481d8daca7ab90774ff37ae81d8e9
lindseydew <lindsey.dew@guardian.co.uk>
2014-01-16 14:30:36 +0000

    parse the json from the post endpoint

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -3,16 +3,37 @@ package controllers
 import play.api._
 import play.api.mvc._
 import models.{NewsItems, NewsItem}
+import play.api.libs.json._
+
+case class ParsedItem(height: Int, width: Int, linkText: String, url: String)
+
+object ParsedItem  {
+  import _root_.play.api.libs.functional.syntax._
+  implicit val reads: Reads[ParsedItem]  = (
+      (__ \"height").read[Int] ~
+        (__ \"width").read[Int] ~
+        (__ \"linkText").read[String] ~
+        (__ \"url").read[String]
+      )(ParsedItem.apply _)
+}
+
 
 object Application extends Controller {
-  
+
   def index = Action {
 
     Ok("hello world")
   }
 
-  def insert = Action { implicit request =>
-    println(request.body.asJson)
+
+  def insert(source: String) = Action { implicit request =>
+    val json = request.body.asJson
+    json match {
+      case Some(JsArray(items)) =>{
+        items.map(_.as[ParsedItem])
+      }
+      case _ => println("couldn't parse json")
+    }
     Ok
   }
   
