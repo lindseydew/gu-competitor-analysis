455dfc397378a0b00e5076b9b488fb34dfc2050c
lindseydew <lindsey.dew@guardian.co.uk>
2014-01-16 13:46:38 +0000

    add a post endpoint to receive json

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -2,11 +2,18 @@ package controllers
 
 import play.api._
 import play.api.mvc._
+import models.{NewsItems, NewsItem}
 
 object Application extends Controller {
   
   def index = Action {
+
     Ok("hello world")
+  }
+
+  def insert = Action { implicit request =>
+    println(request.body.asJson)
+    Ok
   }
   
 }
