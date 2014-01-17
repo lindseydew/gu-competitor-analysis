13924a41711a5dd189fdec8261e47079a20a5e72
lindseydew <lindsey.dew@guardian.co.uk>
2014-01-17 12:17:03 +0000

    refactor

--- app/models/NewsItem.scala
+++ app/models/NewsItem.scala
@@ -6,13 +6,7 @@ case class Position(width: Int, height: Int, xOffset: Int, yOffset: Int) {
 }
 
 case class Source(name: String, items: List[NewsItem])
-case class Story(entities: List[String], article: NewsItem, linkTo: List[NewsItem]=Nil)  {
-  val excludedWords = List("A", "The", "On", "And")
-  def processStory(headline: String): List[String] = {
-    val words = headline.split(" ")
-    (for(word<-words; if(word.head.isUpper && !excludedWords.contains(word))) yield word).toList
-  }
-}
+
 case class NewsItem(headline: String, url: String, entities: List[String], position: Position, linkTo: List[NewsItem]=Nil)
 
 case class Entities(headline: String, entities: List[String])
